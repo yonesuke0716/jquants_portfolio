3eb1b3454054cbdac8a4153981126127d918122c
yonesuke0716 <yone2006521022@yahoo.co.jp>
2024-01-05 11:42:46 +0900

    first-commit

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # jquants_portfolio
 J-Quants APIを使って日本株ポートフォリオを分析するツール
+
+## 準備
+### J-Quants APIアカウントの登録
+
+公式HPでアカウントを作成し、「メールアドレス」と「パスワード」を記録する。
+
+https://jpx-jquants.com/
+
+### user-api.tomlの作成
+
+configフォルダ内に「user-api.toml」という名前でファイルを作成する。
+
+作成したファイル内に、以下のように記述する
+```
+[jquants-api-client]
+mailaddress = "your-email"
+password = "your-password"
+```
+
+"your-email"に「メールアドレス」、"your-password"に「パスワード」をコピペする。
+
+## 開発環境の準備
+### 仮想環境の場合
+
+```
+pip install -r requirements.txt
+```
+
+### Dockerの場合
+
+- docer imageの立ち上げ
+```
+docker compose up -d --build
+```
+- docker containerの起動
+```
+docker compose exec python3 bash
+```
+
+## 実行方法
+
+```
+python src/stock.py
+```
+
+現状は上場株式総数が表示される。
---
+++ src/stock.py
@@ -0,0 +1,46 @@
+import requests
+import json
+import tomllib
+
+# =============================================================
+# API情報取得
+# =============================================================
+config_path = "config/user-api.toml"
+
+with open(config_path, mode="rb") as f:
+    ret = tomllib.load(f)["jquants-api-client"]
+# =============================================================
+# API認証情報取得
+# =============================================================
+# refresh token取得
+try:
+    req_post = requests.post(
+        "https://api.jquants.com/v1/token/auth_user", data=json.dumps(ret)
+    )
+    REFRESH_TOKEN = req_post.json()["refreshToken"]
+except Exception:
+    print("RefreshTokenの取得に失敗しました。")
+# idToken取得
+try:
+    req_post = requests.post(
+        f"https://api.jquants.com/v1/token/auth_refresh?refreshtoken={REFRESH_TOKEN}"
+    )
+    idToken = req_post.json()["idToken"]
+except Exception:
+    print("idTokenの取得に失敗しました。")
+print("API使用の準備が完了しました。")
+# ===============================================================
+# 上場銘柄一覧
+# ===============================================================
+headers = {"Authorization": "Bearer {}".format(idToken)}
+urlPath = "https://api.jquants.com/v1/listed/info"
+req = requests.get(urlPath, headers=headers)
+
+# 取得結果
+result = req.json()
+
+infos = result["info"]
+
+company_names = [d.get("CompanyName") for d in infos]
+# 出力
+print(len(company_names))
